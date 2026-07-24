64a1aa960ac87894ded80d42fe0d14f87d20a698
jfariasro <josefariasromero@hotmail.com>
2026-07-24 17:24:49 -0500

    refactor: remove unused maintenance modules and update dashboard query logic

--- inicio.php
+++ inicio.php
@@ -1,74 +1,44 @@
 <?php
-require_once 'clases/regresionLineal.php';
-// Definimos los datos de Ventas
+// Definimos los datos de Compras por mes
 mysqli_query($con, "SET lc_time_names = 'es_ES'");
 
-$query = "SELECT DATE_FORMAT(fecha, '%M') AS mes, sum(total) as total FROM venta
-WHERE MONTH(fecha) != MONTH(NOW()) AND MONTH(fecha) >= MONTH(NOW()) - 6
-GROUP BY MONTH(fecha)";
-$resVenta = mysqli_query($con, $query);
-$datosVenta = array(
-    array('Mes', 'Ven')
-);
-
-$mes = 1;
-$x = array();
-$y = array();
-while ($row = mysqli_fetch_assoc($resVenta)) {
-    array_push($datosVenta, array($row['mes'], (float) $row['total']));
-    array_push($x, $mes);
-    array_push($y, $row['total']);
-    $mes++;
-}
-
-//Definimos los datos de Compras
-$query = "SELECT DATE_FORMAT(fecha, '%M') AS mes, sum(total) as total FROM compras
+$query = "SELECT DATE_FORMAT(fecha, '%M') AS mes, SUM(total) as total FROM compras
 WHERE MONTH(fecha) != MONTH(NOW()) AND MONTH(fecha) >= MONTH(NOW()) - 6
 GROUP BY MONTH(fecha)";
 $resCompra = mysqli_query($con, $query);
 $datosCompra = array(
-    array('Mes', 'Com')
+    array('Mes', 'Compras')
 );
 
-while ($row = mysqli_fetch_assoc($resCompra)) {
-    array_push($datosCompra, array($row['mes'], (float) $row['total']));
-}
-
-//Definimos los datos para el Control de Predicción
-$ia = new IAphp();
-$prediccionVentas = $ia->regresionLineal($x, $y);
-$w = $prediccionVentas['w'];
-$b = $prediccionVentas['b'];
-$datosPrediccion = array_merge([], $datosVenta);
-$datosPrediccion[0] = array('Mes', 'Ven', 'Prediccion');
-$mes = 1;
-for ($i = 1; $i < count($x) + 11; $i++) {
-    $venta = $w * ($i + 1) + $b;
-
-    if ($mes > count($x)) {
-        setlocale(LC_TIME, 'es_ES.UTF-8', 'es_ES', 'Spanish_Spain');
-
-        $nombre_mes = strftime('%B', mktime(0, 0, 0, $mes, 1));
-        array_push($datosPrediccion, array($nombre_mes, null, $venta));
-    } else {
-        $datosPrediccion[$i] = array($datosPrediccion[$i][0], $datosPrediccion[$i][1], $venta);
+if ($resCompra) {
+    while ($row = mysqli_fetch_assoc($resCompra)) {
+        array_push($datosCompra, array($row['mes'], (float) $row['total']));
     }
-
-    $mes++;
 }
 
-$query = "SELECT COUNT(id) AS num from venta
-where fecha BETWEEN DATE( DATE_SUB(NOW(),INTERVAL 7 DAY) ) AND NOW(); ";
-$resNumVentas = mysqli_query($con, $query);
-$rowNumVentas = mysqli_fetch_assoc($resNumVentas);
+// Conteo de compras totales
+$query = "SELECT COUNT(id) AS num FROM compras;";
+$resNumCompras = mysqli_query($con, $query);
+$rowNumCompras = $resNumCompras ? mysqli_fetch_assoc($resNumCompras) : ['num' => 0];
 
-$query = "SELECT COUNT(id) AS num from clientes; ";
-$resNumClientes = mysqli_query($con, $query);
-$rowNumClientes = mysqli_fetch_assoc($resNumClientes);
+// Conteo de proveedores
+$query = "SELECT COUNT(id) AS num FROM proveedores;";
+$resNumProveedores = mysqli_query($con, $query);
+$rowNumProveedores = $resNumProveedores ? mysqli_fetch_assoc($resNumProveedores) : ['num' => 0];
 
-$query = "SELECT sum(total) AS total FROM venta
-WHERE MONTH(fecha) = MONTH(NOW())";
+// Conteo de insumos
+$query = "SELECT COUNT(id) AS num FROM insumos;";
+$resNumInsumos = mysqli_query($con, $query);
+$rowNumInsumos = $resNumInsumos ? mysqli_fetch_assoc($resNumInsumos) : ['num' => 0];
+
+// Conteo de usuarios
+$query = "SELECT COUNT(id) AS num FROM usuarios;";
+$resNumUsuarios = mysqli_query($con, $query);
+$rowNumUsuarios = $resNumUsuarios ? mysqli_fetch_assoc($resNumUsuarios) : ['num' => 0];
+
+// Total compras del mes actual
+$query = "SELECT SUM(total) AS total FROM compras WHERE MONTH(fecha) = MONTH(NOW()) AND YEAR(fecha) = YEAR(NOW());";
 $resActual = mysqli_query($con, $query);
-$rowActual = mysqli_fetch_assoc($resActual);
+$rowActual = $resActual ? mysqli_fetch_assoc($resActual) : ['total' => 0];
 
 require 'views/inicio.view.php';
--- views/inicio.view.php
+++ views/inicio.view.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Panel de Control</h1>
+                    <h1 class="m-0 text-dark">Panel de Control - Módulo de Compras</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -16,109 +16,106 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-4">
+                <div class="col-md-3 col-sm-6 col-12">
                     <!-- small box -->
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3><?php echo $rowNumVentas['num'] ?? 0; ?></h3>
-                            <p>Ventas en los ultimos 7 dias</p>
+                            <h3><?php echo $rowNumCompras['num'] ?? 0; ?></h3>
+                            <p>Total Compras Registradas</p>
                         </div>
                         <div class="icon">
-                            <i class="ion ion-bag"></i>
+                            <i class="fas fa-shopping-cart"></i>
                         </div>
-                        <a href="panel.php?modulo=ventas" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="panel.php?modulo=compras" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
-                <div class="col-4">
+                <div class="col-md-3 col-sm-6 col-12">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3><?php echo $rowNumInsumos['num'] ?? 0; ?></h3>
+                            <p>Insumos Registrados</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-seedling"></i>
+                        </div>
+                        <a href="panel.php?modulo=insumos" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-md-3 col-sm-6 col-12">
                     <!-- small box -->
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3><?php echo $rowNumClientes['num'] ?? 0; ?></h3>
-                            <p>Clientes</p>
+                            <h3><?php echo $rowNumProveedores['num'] ?? 0; ?></h3>
+                            <p>Proveedores Activos</p>
                         </div>
                         <div class="icon">
-                            <i class="ion ion-person-add"></i>
+                            <i class="fas fa-user-check"></i>
                         </div>
-                        <a href="panel.php?modulo=clientes" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="panel.php?modulo=proveedores" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
-                <!-- ./col -->
-                <div class="col-4">
+                <div class="col-md-3 col-sm-6 col-12">
                     <!-- small box -->
                     <div class="small-box bg-primary">
                         <div class="inner">
-                            <h3><?php echo '$' . $rowActual['total']; ?></h3>
-                            <p>Total de Ventas en el Mes Actual</p>
+                            <h3><?php echo '$' . number_format($rowActual['total'] ?? 0, 2); ?></h3>
+                            <p>Compras Mes Actual</p>
                         </div>
                         <div class="icon">
-                            <i class="ion ion-ios-eye"></i>
+                            <i class="fas fa-file-invoice-dollar"></i>
                         </div>
-                        <a href="panel.php?modulo=cosecha" class="small-box-footer">Nueva Venta <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="panel.php?modulo=generar-compra" class="small-box-footer">Nueva Compra <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
             </div>
+        </div>
     </section>
 
     <section class="content">
         <div class="container-fluid">
-
             <div class="row">
-                <div class="col-xl-6">
-                    <div id="chart_div" style="height: 500px;">
-                    </div>
-                </div>
-                <div class="col-xl-6">
-                    <div id="chart_div2" style="height: 500px;">
+                <div class="col-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h3 class="card-title"><i class="fas fa-chart-bar mr-1"></i> Histórico de Compras por Mes</h3>
+                        </div>
+                        <div class="card-body">
+                            <div id="chart_div_compras" style="height: 400px; width: 100%;"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
-</div>
-
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header bg-light py-3">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-4 text-left">
-                    <h1 class="m-1 text-dark text-center">Control Predicción</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-        <div class="container">
-            <div class="card card-primary card-outline">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <canvas id="myChart"></canvas>
-                        </div>
-                    </div>
-                </div><!-- /.card-body -->
-            </div><!-- /.card -->
-        </div><!-- /.container -->
-    </section>
-    <!-- /.content -->
 </div><!-- /.content-wrapper -->
 
+<script type="text/javascript">
+    if (typeof google !== 'undefined' && google.charts) {
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+    }
 
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(<?php echo json_encode($datosCompra); ?>);
 
+        var options = {
+            title: 'Resumen de Compras por Mes',
+            curveType: 'function',
+            legend: { position: 'bottom' },
+            colors: ['#28a745'],
+            vAxis: { format: 'currency' }
+        };
 
-<script>
-    $(document).ready(function() {
-        Grafica(<?php echo json_encode($datosVenta); ?>, 'Ventas por mes', 'chart_div');
-        Grafica(<?php echo json_encode($datosCompra); ?>, 'Compras por mes', 'chart_div2');
-        Graficar(<?php echo json_encode($datosPrediccion); ?>);
-    });
+        var chartContainer = document.getElementById('chart_div_compras');
+        if (chartContainer) {
+            var chart = new google.visualization.ColumnChart(chartContainer);
+            chart.draw(data, options);
+        }
+    }
 </script>
-
-<script src="js/graficos/venta.js"></script>
